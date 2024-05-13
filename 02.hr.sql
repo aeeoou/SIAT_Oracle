c9acfbb5e4fffeb6149f239ee3a688ab74d92d8b
verylong <147506109+aeeoou@users.noreply.github.com>
2024-05-13 23:36:43 +0900

    sql 2day 1
    .

--- 02.hr.sql
+++ 02.hr.sql
@@ -11,3 +11,17 @@ SELECT last_name, salary FROM employees WHERE salary <= 3000;
 
 -- 과제 : 월급 $12,000 이상 버는 사원들의 이름, 월급을 조회하라.
 SELECT last_name, salary FROM employees WHERE salary >= 12000;
+
+-- 연산자 != (다르다)
+SELECT last_name, job_id
+FROM employees
+WHERE job_id != 'IT_PROG'; -- 프로그래머가 아니다. 즉 직업이 프로그래머가 아닌 사람을 찾는다.
+
+-- 연산자 between 값 AND 값 (범위를 나타낸다.)
+SELECT last_name, salary
+FROM employees
+WHERE salary BETWEEN 2500 AND 3500; -- 시작값(포함) and 끝값(포함) 사이를 찾는다.
+
+SELECT last_name
+FROM employees
+WHERE last_name BETWEEN 'King' AND 'Smith'; -- 캐릭터 값으로 찾기
